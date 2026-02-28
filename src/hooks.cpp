39bc1227a251fb1cdcc249be96f9042f7de3c0d6
Matthew <17109660+matty45@users.noreply.github.com>
2026-02-28 00:29:21 +0000

    starting to get into the tutorial

--- src/hooks.cpp
+++ src/hooks.cpp
@@ -1,29 +1,29 @@
 #include <rex/ppc/types.h>
 
 
-//https://github.com/rexglue/rexglue-sdk/issues/121
-void FixBrokenSwitchTable_82812FA4(PPCRegister& r3) {
+// //https://github.com/rexglue/rexglue-sdk/issues/121
+// void FixBrokenSwitchTable_82812FA4(PPCRegister& r3) {
 
-	switch (r3.u32) {
-	case 0:
-		r3.s64 = 2;
-		break;
-	case 1:
-		r3.s64 = 1;
-		break;
-	default:
-		r3.s64 = 1;
-		break;
-	case 3:
-		r3.s64 = 0;
-		break;
-	case 2:
-		r3.s64 = 3;
-		break;
-	case 0xc:
-		r3.s64 = 3;
-		break;
-	}
-}
+// 	switch (r3.u32) {
+// 	case 0:
+// 		r3.s64 = 2;
+// 		break;
+// 	case 1:
+// 		r3.s64 = 1;
+// 		break;
+// 	default:
+// 		r3.s64 = 1;
+// 		break;
+// 	case 3:
+// 		r3.s64 = 0;
+// 		break;
+// 	case 2:
+// 		r3.s64 = 3;
+// 		break;
+// 	case 0xc:
+// 		r3.s64 = 3;
+// 		break;
+// 	}
+// }
 
 

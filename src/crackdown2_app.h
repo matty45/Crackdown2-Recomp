6ad4f75613e64299b229e71594fe552d5287c8fd
Matthew <17109660+matty45@users.noreply.github.com>
2026-03-11 19:27:05 +0000

    Update main cpp

---
+++ src/crackdown2_app.h
@@ -0,0 +1,26 @@
+// crackdown2 - ReXGlue Recompiled Project
+//
+// This file is yours to edit. 'rexglue migrate' will NOT overwrite it.
+// Customize your app by overriding virtual hooks from rex::ReXApp.
+
+#pragma once
+
+#include <rex/rex_app.h>
+
+class Crackdown2App : public rex::ReXApp {
+ public:
+  using rex::ReXApp::ReXApp;
+
+  static std::unique_ptr<rex::ui::WindowedApp> Create(
+      rex::ui::WindowedAppContext& ctx) {
+    return std::unique_ptr<Crackdown2App>(new Crackdown2App(ctx, "crackdown2",
+        PPCImageConfig));
+  }
+
+  // Override virtual hooks for customization:
+  // void OnPreSetup(rex::RuntimeConfig& config) override {}
+  // void OnPostSetup() override {}
+  // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
+  // void OnShutdown() override {}
+  // void OnConfigurePaths(rex::PathConfig& paths) override {}
+};

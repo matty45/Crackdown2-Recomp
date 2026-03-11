14c5b58288b7d65bbd436b80fbabdd2bdf02dfd3
Matthew <17109660+matty45@users.noreply.github.com>
2026-03-11 23:23:59 +0000

    implement rexcrt hooks

--- src/crackdown2_app.h
+++ src/crackdown2_app.h
@@ -8,19 +8,19 @@
 #include <rex/rex_app.h>
 
 class Crackdown2App : public rex::ReXApp {
- public:
-  using rex::ReXApp::ReXApp;
+public:
+    using rex::ReXApp::ReXApp;
 
-  static std::unique_ptr<rex::ui::WindowedApp> Create(
-      rex::ui::WindowedAppContext& ctx) {
-    return std::unique_ptr<Crackdown2App>(new Crackdown2App(ctx, "crackdown2",
-        PPCImageConfig));
-  }
+    static std::unique_ptr<rex::ui::WindowedApp> Create(
+        rex::ui::WindowedAppContext& ctx) {
+        return std::unique_ptr<Crackdown2App>(new Crackdown2App(ctx, "crackdown2",
+            PPCImageConfig));
+    }
 
-  // Override virtual hooks for customization:
-  // void OnPreSetup(rex::RuntimeConfig& config) override {}
-  // void OnPostSetup() override {}
-  // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
-  // void OnShutdown() override {}
-  // void OnConfigurePaths(rex::PathConfig& paths) override {}
+    // Override virtual hooks for customization:
+    // void OnPreSetup(rex::RuntimeConfig& config) override {}
+    // void OnPostSetup() override {}
+    // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
+    // void OnShutdown() override {}
+    // void OnConfigurePaths(rex::PathConfig& paths) override {}
 };
